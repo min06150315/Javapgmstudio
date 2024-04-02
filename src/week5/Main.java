d684961c629829c29e4894b4c26305ff1420b091
min06150315 <min06150315@gmail.com>
2024-04-02 11:42:15 +0900

    Practice Java

--- src/week5/Main.java
+++ src/week5/Main.java
@@ -3,8 +3,21 @@ package week5;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        Main a = new Main();
-        a.J001();
+        Main pStudio = new Main();
+        pStudio.J001();
     }
-    void J001(){ System.out.println("test");}
+    void J001()
+    {
+        int height, weight;
+        double bmi;
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter the height and weight (cm, kg): ");
+        height = keyboard.nextInt();
+        weight = keyboard.nextInt();
+
+        bmi = weight / ((height * 0.01) * (height * 0.01));
+
+        System.out.print("bmi: " + bmi);
+    }
 }
