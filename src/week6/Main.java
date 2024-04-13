bdefcb4bad6860d64fde54df33c02fe221deb81f
min06150315 <min06150315@gmail.com>
2024-04-13 22:24:46 +0900

    Add Main.java

---
+++ src/week6/Main.java
@@ -0,0 +1,263 @@
+package week6;
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Main pStudio = new Main();
+        Scanner keyboard = new Scanner(System.in);
+
+        // 원하는 문제 번호 입력받아서 보여주기
+        int num;
+        while (true) {
+            System.out.print("Enter the Problem Number: ");
+            num = keyboard.nextInt();
+
+            if (num == 21) pStudio.J021();
+            else if (num == 22) pStudio.J022();
+            else if (num == 23) pStudio.J023();
+            else if (num == 24) pStudio.J024();
+            else if (num == 25) pStudio.J025();
+            else if (num == 26) pStudio.J026();
+            else if (num == 27) pStudio.J027();
+            else if (num == 28) pStudio.J028();
+            else if (num == 29) pStudio.J029();
+            else if (num == 30) pStudio.J030();
+            else break;
+        }
+    }
+    public void J021()
+    {
+        int people = 0;
+        Scanner keyboard = new Scanner(System.in);
+
+        int count = keyboard.nextInt();
+        int[] height = new int[count];
+        int[] weight = new int[count];
+        double[] bmi = new double[count];
+
+        for (int i = 0; i < count; i++){
+            height[i] = keyboard.nextInt();
+            weight[i] = keyboard.nextInt();
+            bmi[i] = weight[i] / Math.pow(height[i] * 0.01, 2);
+            if (bmi[i] >= 25)
+                people++;
+        }
+        System.out.println(people);
+    }
+    public void J022()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        double[] m2_area = new double[10];
+        double[] pyung_area = new double[10];
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        int count4 = 0;
+
+        for (int i = 0; i < 10; i++) {
+            m2_area[i] = keyboard.nextDouble();
+        }
+        for (int i = 0; i < 10; i++) {
+            pyung_area[i] = m2_area[i] / 3.305;
+            if (pyung_area[i] < 15) count1++;
+            else if (pyung_area[i] < 30) count2++;
+            else if (pyung_area[i] < 50) count3++;
+            else count4++;
+        }
+
+        System.out.println("small - " + count1);
+        System.out.println("normal - " + count2);
+        System.out.println("large - " + count3);
+        System.out.println("huge - " + count4);
+    }
+    public void J023()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int count;
+        int totalSum = 0;
+        double average;
+
+        count = keyboard.nextInt();
+        int[] number = new int[count];
+
+        for (int i = 0; i < count; i++){
+            number[i] = keyboard.nextInt();
+            totalSum += number[i];
+        }
+
+        average = (double) totalSum / count;
+        System.out.print(totalSum + " " + average);
+    }
+    public void J024()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int[] numbers = new int[10];
+        int first;
+        int second;
+        int second_max_index = 0;
+
+        // 10개의 숫자 입력받기
+        for (int i = 0; i < 10; i++)
+            numbers[i] = keyboard.nextInt();
+
+        // 출력을 위한 배열 복사본 만들어 놓기
+        int[] num_temp = numbers.clone();
+
+        // 배열 정렬하기
+        Arrays.sort(numbers);
+
+        // 첫 번째, 두 번째로 큰 수 구하기
+        first = numbers[9];
+        second = numbers[8];
+
+        // 두 번째로 큰 수의 인덱스 구하기
+        for (int i = 0; i < 10; i++)
+            if (num_temp[i] == second)
+                second_max_index = i;
+
+        // 결과 출력하기
+        System.out.println("[" + (second_max_index + 1) + "] " + second);
+    }
+    public void J025()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int[] monthdays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int month, day;
+        int day_count = 0;
+
+        month = keyboard.nextInt();
+        day = keyboard.nextInt();
+
+        // 오류 판정
+        int result = 0;
+        if (month < 1 || month > 12) {
+            System.out.println("Wrong date!");
+            result = -1;
+        }else if (month == 2 && day > 28){
+            System.out.println("Wrong date!");
+            result = -1;
+        }else if (day > monthdays[month - 1]){
+            System.out.println("Wrong date!");
+            result = -1;
+        }
+
+        // 연중 날짜 계산하고 출력하기
+        if (result == 0) {
+            for (int i = 0; i < month - 1; i++)
+                day_count += monthdays[i];
+            day_count += day;
+            System.out.println(day_count);
+        }
+    }
+    public void J026()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int num;
+        int max_num, min_num;
+
+        num = keyboard.nextInt();
+        int[] number = new int[num];
+
+        for (int i = 0; i < num; i++)
+            number[i] = keyboard.nextInt();
+
+        Arrays.sort(number);
+        max_num = number[num - 1];
+        min_num = number[0];
+
+        System.out.println("Biggest " + max_num);
+        System.out.println("Smallest " + min_num);
+    }
+    public void J027()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int[] height = new int[10];
+        int[] weight = new int[10];
+        double[] bmi = new double[10];
+        int count = 0;
+        int i;
+
+        int num_count = keyboard.nextInt();
+        for (i = 0; i < num_count; i++) {
+            height[i] = keyboard.nextInt();
+            weight[i] = keyboard.nextInt();
+            bmi[i] = weight[i] / Math.pow(height[i] * 0.01, 2);
+        }
+
+        System.out.print("Overweight person - ");
+        for (i = 0; i < num_count; i++) {
+            if (bmi[i] >= 25) {
+                System.out.print((i + 1) + " ");
+                count++;
+            }
+        }
+        System.out.println("\nTotal - " + count);
+    }
+    public void J028()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#.#");
+        int[][] jumsu = new int[5][3];  // 5명의 3과목 점수를 저장하고 있는 2차원 배열
+        int[] sum_student = new int[5]; // 학생별 총점
+        double[] average_student = new double[5];   // 학생별 평균
+        char[] grade = new char[5];     // 학생별 등급
+        int[] sum_grade = new int[3];   // 과목별 총점
+        double[] average_class = new double[3]; // 과목별 평균
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                jumsu[i][j] = keyboard.nextInt();   // 점수 입력 받기
+                sum_student[i] += jumsu[i][j];      // 학생별 총합에 더하기
+            }
+            average_student[i] = (double) sum_student[i] / 3;   // 학생별 평균 구하기
+            average_student[i] = Double.parseDouble(df.format(average_student[i])); // 소수점 1자리 까지 반올림
+            if (average_student[i] >= 90) grade[i] = 'A';       // 학생별 평균 으로 학생별 등급 판정하기
+            else if (average_student[i] >= 80) grade[i] = 'B';
+            else if (average_student[i] >= 70) grade[i] = 'C';
+            else if (average_student[i] >= 60) grade[i] = 'D';
+            else grade[i] = 'F';
+        }
+        // 과목별 총점, 평균 구하기
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++)
+                sum_grade[i] += jumsu[j][i];
+            average_class[i] = (double) sum_grade[i] / 5;
+            average_class[i] = Double.parseDouble(df.format(average_class[i]));
+        }
+    }
+    public void J029()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int[] number = new int[10];     // 사용자가 입력한 숫자 10개
+        int count = 0;      // 현재까지 입력된 숫자의 개수(0~10)
+
+        while (true) {
+            int result = 0;
+            System.out.print("Enter #" + (count + 1) + " number > ");
+            number[count] = keyboard.nextInt();
+
+            for (int i = 0; i < count; i++) {   // 숫자가 기존 숫자들과 중복되는지 비교하기
+                if (number[count] == number[i]) {
+                    System.out.println("Duplicated! Retry.");
+                    result = -1;
+                }
+            }
+            if (result == 0)    // 중복이 없을 시 다음 입력 받을 준비 하기
+                count++;
+
+            if (count == 10) {      // 10개의 수를 모두 입력 하면 결과 출력
+                System.out.print("Numbers -");
+                for (int i = 0; i < 10; i++)
+                    System.out.print(" " + number[i]);
+                break;
+            }
+        }
+    }
+    public void J030()
+    {
+
+    }
+}
