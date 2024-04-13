2c1f56bcd837e9f4f90a62a498fbf04919801481
min06150315 <min06150315@gmail.com>
2024-04-13 19:09:36 +0900

    Update Java023.java

--- src/Practice/JavaLab3/Java023.java
+++ src/Practice/JavaLab3/Java023.java
@@ -8,25 +8,22 @@ public class Java023
         Java023 pStudio = new Java023();
         pStudio.J023();
     }
-
     public void J023()
     {
         Scanner keyboard = new Scanner(System.in);
         int count;
-        int totalsum = 0;
+        int totalSum = 0;
         double average;
 
-        System.out.print("Enter the count of number: ");
         count = keyboard.nextInt();
         int[] number = new int[count];
 
-        System.out.print("Enter the " + count + " numbers: ");
         for (int i = 0; i < count; i++){
             number[i] = keyboard.nextInt();
-            totalsum += number[i];
+            totalSum += number[i];
         }
 
-        average = (double) totalsum / count;
-        System.out.print(totalsum + " " + average);
+        average = (double) totalSum / count;
+        System.out.print(totalSum + " " + average);
     }
 }
