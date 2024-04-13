84cd542c4f20996fcd6ea2536000a83363bcacaf
min06150315 <min06150315@gmail.com>
2024-04-13 19:09:26 +0900

    Update Java021.java

--- src/Practice/JavaLab3/Java021.java
+++ src/Practice/JavaLab3/Java021.java
@@ -13,28 +13,18 @@ public class Java021
         int people = 0;
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.print("Enter the count of people: ");
         int count = keyboard.nextInt();
         int[] height = new int[count];
         int[] weight = new int[count];
         double[] bmi = new double[count];
 
         for (int i = 0; i < count; i++){
-            System.out.print("Enter the height and weight (cm, kg): ");
             height[i] = keyboard.nextInt();
             weight[i] = keyboard.nextInt();
             bmi[i] = weight[i] / Math.pow(height[i] * 0.01, 2);
-            if (bmi[i] >= 25) {
+            if (bmi[i] >= 25)
                 people++;
-            }
         }
-        for (int i = 0; i < count; i++){
-            if (bmi[i] >= 25) {
-                System.out.println(height[i] + " " + weight[i] + ": O");
-            }else {
-                System.out.println(height[i] + " " + weight[i] + ": X");
-            }
-        }
-        System.out.println("BMI People: " + people);
+        System.out.println(people);
     }
 }
