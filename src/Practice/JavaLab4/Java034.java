574156951b1ba3323b0f44f31e54135226599e18
min06150315 <min06150315@gmail.com>
2024-04-17 00:10:24 +0900

    Update Java034.java

--- src/Practice/JavaLab4/Java034.java
+++ src/Practice/JavaLab4/Java034.java
@@ -11,5 +11,27 @@ public class Java034
     public void J034()
     {
         Scanner keyboard = new Scanner(System.in);
+        String[] usernames = {"kim", "lee", "park", "hong", "choi"};
+        String[] passwords = {"1111", "1234", "3456", "3535", "7777"};
+
+        String name;
+        String PW;
+
+        name = keyboard.next();
+        PW = keyboard.next();
+
+        boolean found = false;
+
+        for (int i = 0; i < usernames.length; i++) {
+            if (name.equals(usernames[i])) {
+                found = true;
+                if (PW.equals(passwords[i]))
+                    System.out.println("Login OK!");
+                else
+                    System.out.println("Incorrect password!");
+            }
+        }
+        if (!found)
+            System.out.println("No user!");
     }
 }
