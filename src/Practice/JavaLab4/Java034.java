cc26dc32196f2980314578bd81f0db1354d9525a
min06150315 <min06150315@gmail.com>
2024-04-16 23:31:29 +0900

    Add Java034.java

---
+++ src/Practice/JavaLab4/Java034.java
@@ -0,0 +1,15 @@
+// J034. 회원로그인 아이디/비번 검사하기
+package Practice.JavaLab4;
+import java.util.Scanner;
+public class Java034
+{
+    public static void main(String[] args)
+    {
+        Java034 pStudio = new Java034();
+        pStudio.J034();
+    }
+    public void J034()
+    {
+        Scanner keyboard = new Scanner(System.in);
+    }
+}
