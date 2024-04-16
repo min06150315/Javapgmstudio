2fec0960fab0a161955a0addf5402af71b11f694
min06150315 <min06150315@gmail.com>
2024-04-16 23:31:39 +0900

    Add Java036.java

---
+++ src/Practice/JavaLab4/Java036.java
@@ -0,0 +1,15 @@
+// J036. 희문검사하기
+package Practice.JavaLab4;
+import java.util.Scanner;
+public class Java036
+{
+    public static void main(String[] args)
+    {
+        Java036 pStudio = new Java036();
+        pStudio.J036();
+    }
+    public void J036()
+    {
+        Scanner keyboard = new Scanner(System.in);
+    }
+}
