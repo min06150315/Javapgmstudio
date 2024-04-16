293c7915f4c9ff6d325e0329ead2ff1987b86339
min06150315 <min06150315@gmail.com>
2024-04-16 23:31:49 +0900

    Add Java037.java

---
+++ src/Practice/JavaLab4/Java037.java
@@ -0,0 +1,15 @@
+// J037. 애너그램 판별하기
+package Practice.JavaLab4;
+import java.util.Scanner;
+public class Java037
+{
+    public static void main(String[] args)
+    {
+        Java037 pStudio = new Java037();
+        pStudio.J037();
+    }
+    public void J037()
+    {
+        Scanner keyboard = new Scanner(System.in);
+    }
+}
