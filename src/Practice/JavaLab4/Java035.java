4a148e726ed3b9093e546e181b919bd8fa1cf685
min06150315 <min06150315@gmail.com>
2024-04-16 23:31:35 +0900

    Add Java035.java

---
+++ src/Practice/JavaLab4/Java035.java
@@ -0,0 +1,15 @@
+// J035. 생년월일을 분해해 숫자 만들기
+package Practice.JavaLab4;
+import java.util.Scanner;
+public class Java035
+{
+    public static void main(String[] args)
+    {
+        Java035 pStudio = new Java035();
+        pStudio.J035();
+    }
+    public void J035()
+    {
+        Scanner keyboard = new Scanner(System.in);
+    }
+}
