c696363c4b6cf8164c86d054d5a984412c3d1684
min06150315 <min06150315@gmail.com>
2024-04-27 22:33:19 +0900

    Update J035

--- src/week7/Main.java
+++ src/week7/Main.java
@@ -152,7 +152,26 @@ public class Main
     }
     public void J036()
     {
+        Scanner keyboard = new Scanner(System.in);
+        String str;
+        StringBuilder reverse = new StringBuilder();
 
+        str = keyboard.nextLine();
+        str = str.toLowerCase();
+
+        // 문자 이외의 공백과 기호 없애기
+        str = str.replaceAll("[^a-z0-9 ]", "");
+        str = str.replaceAll("\\s+", "");
+
+        int len = str.length();
+
+        for (int i = len - 1; i >= 0; i--)
+            reverse.append(str.charAt(i));
+
+        if (str.equalsIgnoreCase(reverse.toString()))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
     }
     public void J037()
     {
