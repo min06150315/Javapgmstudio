3b7f6c7ea7b6398b80bc8c8bf00c5d9b8d2f7a1c
min06150315 <min06150315@gmail.com>
2024-04-27 22:33:05 +0900

    Update J035

--- src/week7/Main.java
+++ src/week7/Main.java
@@ -133,7 +133,22 @@ public class Main
     }
     public void J035()
     {
+        Scanner keyboard = new Scanner(System.in);
+        String birthday;
+        String year, month, day;
+        int sum = 0;
 
+        birthday = keyboard.next();
+        year = birthday.substring(0, 4);
+        month = birthday.substring(4, 6);
+        day = birthday.substring(6, 8);
+
+        for (int i = 0; i < 4; i++)
+            sum += Integer.parseInt(year.substring(i, i + 1));
+
+        sum += Integer.parseInt(month) + Integer.parseInt(day);
+
+        System.out.println(sum);
     }
     public void J036()
     {
