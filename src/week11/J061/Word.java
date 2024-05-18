1e4e36690ffbd8334ca2ede89eec8712934bdc15
min06150315 <min06150315@gmail.com>
2024-05-18 23:25:32 +0900

    Update J061

--- src/week11/J061/Word.java
+++ src/week11/J061/Word.java
@@ -1,4 +1,33 @@
 package week11.J061;
 
 public class Word {
+    private String eng_word;    // 영문 단어
+    private String kor_Word;    // 한글 뜻
+    private int difficulty;     // 난이도 (1 ~ 3)
+    Word(String eng_word, String kor_Word, int difficulty) {
+        this.eng_word = eng_word;
+        this.kor_Word = kor_Word;
+        this.difficulty = difficulty;
+    }
+    public String getEng_word() {
+        return eng_word;
+    }
+    public String getKor_Word() {
+        return kor_Word;
+    }
+    public int getDifficulty() {
+        return difficulty;
+    }
+    public void setEng_word(String eng_word) {
+        this.eng_word = eng_word;
+    }
+    public void setKor_Word(String kor_Word) {
+        this.kor_Word = kor_Word;
+    }
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+    public String toString() {      // 멤버의 내용을 문자열로 리턴
+        return String.format("%s [%d] : %s", eng_word, difficulty, kor_Word);
+    }
 }
--- src/week11/J061/WordCRUD.java
+++ src/week11/J061/WordCRUD.java
@@ -1,4 +1,61 @@
 package week11.J061;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class WordCRUD {
+    ArrayList<Word> list;
+    public WordCRUD() {
+        list = new ArrayList<Word>();
+    }
+    void addWord() {    // 단어 추가
+        String english, korean;
+        int level;
+        Scanner s = new Scanner(System.in);
+        System.out.print("English > ");
+        english = s.next();
+        s.nextLine();
+        System.out.print("Korean > ");
+        korean = s.nextLine();
+        System.out.print("Level (1~3) > ");
+        level = s.nextInt();
+        Word new_word = new Word(english, korean, level);
+        list.add(new_word);
+    }
+    void editWord() {   // 단어 뜻 수정
+        int num;
+        String new_korean;
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter the Number of Word: ");
+        num = s.nextInt() - 1;
+
+        if (num >= 0 && num < list.size()) {
+            System.out.print("New Meaning: ");
+            new_korean = s.next();
+            list.get(num).setKor_Word(new_korean);
+            System.out.println("Edit Complete.");
+        } else {
+            System.out.println("No Data!!");
+        }
+
+    }
+    void listWord() {   // 단어 목록 출력
+        for (int i = 0; i < list.size(); i++)
+            System.out.printf("%d - %s\n", i + 1, list.get(i).toString());
+    }
+    void deleteWord() {   // 단어 삭제
+        int num;
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter the Number of Word: ");
+        num = s.nextInt() - 1;
+
+        if (num >= 0 && num < list.size()) {
+            list.remove(num);
+            System.out.println("Delete Complete.");
+        } else {
+            System.out.println("No Data!!");
+        }
+    }
 }
