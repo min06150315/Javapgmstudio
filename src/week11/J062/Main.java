fe02af1aaf807165800498fc8f5c88f519dbef5e
min06150315 <min06150315@gmail.com>
2024-05-18 23:26:11 +0900

    Update J062

--- src/week11/J062/Main.java
+++ src/week11/J062/Main.java
@@ -1,5 +1,28 @@
 // J062. 도서 관리 프로그램 ver.1
 package week11.J062;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Main Pstudio = new Main();
+        Pstudio.run();
+    }
+    public void run() {
+        int menu, quit = 0;
+        Scanner s = new Scanner(System.in);
+        BookCRUD manager = new BookCRUD();
+        while (true) {
+            System.out.println("Menu > 1.Add 2.Edit 3.List 4.Delete 0.Exit");
+            menu = s.nextInt();
+            switch (menu) {
+                case 1: manager.addBook(); break;
+                case 2: manager.editBook(); break;
+                case 3: manager.listBook(); break;
+                case 4: manager.deleteBook(); break;
+                default: quit = 1;
+            }
+            if (quit == 1) break;
+        }
+    }
 }
