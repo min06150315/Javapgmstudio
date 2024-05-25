b01c4e8fffba179f69c0010449b8220c469e82a4
min06150315 <min06150315@gmail.com>
2024-05-25 22:22:14 +0900

    Update iCRUD.java

---
+++ src/week12/J071/iCRUD.java
@@ -0,0 +1,9 @@
+package week12.J071;
+
+public interface iCRUD {
+    public Object createItem();
+    public int addItem();
+    public int updateItem();
+    public int deleteItem();
+    public int printItem();
+}
