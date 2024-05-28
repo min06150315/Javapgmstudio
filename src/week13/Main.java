a40733f677b2ce7655330a72d981a5b4a729c3ae
min06150315 <min06150315@gmail.com>
2024-05-29 01:26:01 +0900

    Update Main.java

--- src/week13/Main.java
+++ src/week13/Main.java
@@ -11,21 +11,19 @@ public class Main {
 
         // 원하는 문제 번호 입력받아서 보여주기
         int num;
-        while (true) {
-            System.out.print("\nEnter the Problem Number: ");
-            num = keyboard.nextInt();
+        System.out.print("\nEnter the Problem Number: ");
+        num = keyboard.nextInt();
 
-            if (num == 81) pStudio.J081();
-            else if (num == 82) pStudio.J082();
-            else if (num == 83) pStudio.J083();
-            else if (num == 84) pStudio.J084();
-            else if (num == 85) pStudio.J085();
-            else break;
-        }
+        if (num == 81) pStudio.J081();
+        else if (num == 82) pStudio.J082();
+        else if (num == 83) pStudio.J083();
+        else if (num == 84) pStudio.J084();
+        else if (num == 85) pStudio.J085();
     }
     void J081() {
         // J081. 최댓값과 최솟값 구하기 (파일 버전)
-        String filename = "./src/week13/data081_1.txt";
+        String filename = "./src/week13/TextFiles/data081_1.txt";
+        //String filename = "./src/week13/TextFiles/data081_2.txt";
         Scanner inputStream = null;
         int[] numbers = new int[10];
         int count = 0;
@@ -53,7 +51,8 @@ public class Main {
     }
     void J082() {
         // J082. 비만인 사람 비율 알아내기 (파일 버전)
-        String filename = "./src/week13/data082_1.txt";
+        String filename = "./src/week13/TextFiles/data082_1.txt";
+        //String filename = "./src/week13/TextFiles/data082_2.txt";
         Scanner inputStream = null;
         int[] height = new int[5];
         int[] weigth = new int[5];
@@ -86,7 +85,8 @@ public class Main {
     void J083() {
         // J083. 국영수 점수 분석하기 (파일 버전)
         ScoreManager manager = new ScoreManager();
-        String filename = "./src/week13/data083_1.txt";
+        //String filename = "./src/week13/TextFiles/data083_1.txt";
+        String filename = "./src/week13/TextFiles/data083_2.txt";
         Scanner inputStream = null;
         try {
             inputStream = new Scanner(new File(filename));
@@ -114,7 +114,8 @@ public class Main {
     }
     void J084() {
         // J084. 학부 영문이름 중 가장 긴 이름 찾기
-        String filename = "./src/week13/name_!.txt";
+        //String filename = "./src/week13/TextFiles/data084_1.txt";
+        String filename = "./src/week13/TextFiles/data084_2.txt";
         Scanner inputStream = null;
         String[] majors = new String[13];
         int count = 0;
@@ -146,17 +147,18 @@ public class Main {
         System.out.print("Longset name : ");
         for (int i = 0; i < count; i++) {
             if (majors[i].length() == maxLength)
-                System.out.print(majors[i]);
+                System.out.print(majors[i] + " ");
         }
         System.out.print("\nShortest name : ");
         for (int i = 0; i < count; i++) {
             if (majors[i].length() == minLength)
-                System.out.print(majors[i]);
+                System.out.print(majors[i] + " ");
         }
     }
     void J085() {
         // J085. 텍스트 파일 내의 알파벳(대소문자열)의 개수와 빈칸의 개수 구하기
-        String filename = "./src/week13/data085_1.txt";
+        //String filename = "./src/week13/TextFiles/data085_1.txt";
+        String filename = "./src/week13/TextFiles/data085_2.txt";
         Scanner inputStream = null;
         int lowerCount = 0, upperCount = 0, spaceCount = 0;
 
